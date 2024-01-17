ba2c914e2e8bfea96b9c0f1f422ceb2ffde37870
sahik7 <sahikahsan7@gmail.com>
2024-01-17 11:35:52 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MainLayout from './components/layout/MainLayout'
+
+function App() {
+
+  return (
+    <>
+      <MainLayout />
+    </>
+  )
+}
+
+export default App
