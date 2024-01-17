0a4da5b11c1416b7ecf8c9eb5deb88a465cb608f
sahik7 <sahikahsan7@gmail.com>
2024-01-17 12:32:02 +0600

    admin path created

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -7,7 +7,7 @@ import Register from "../pages/Register";
 
 const router = createBrowserRouter([
     {
-        path: "/",
+        path: "/admin",
         element: <App />,
         children: [
             {
