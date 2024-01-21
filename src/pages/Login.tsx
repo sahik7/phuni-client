2fc2a63a649c8c8143b609f6b0f738e7d7c5ae3b
sahik7 <sahikahsan7@gmail.com>
2024-01-21 14:23:09 +0600

    onSubmit function created

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,8 +4,10 @@ function Login() {
 
     const { register, handleSubmit } = useForm()
 
+    const onSubmit = () => { }
+
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit()}>
             <div>
                 <label htmlFor="id">ID:</label>
                 <input type="text" id="id" />
