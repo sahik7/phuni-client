d299756dc0cb1305e91e9718b68fd7e1cfb361c0
sahik7 <sahikahsan7@gmail.com>
2024-01-22 12:11:49 +0600

    persist gate imported

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,14 @@ import { RouterProvider } from "react-router-dom"
 import router from './routes/routes.tsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/features/store.ts'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/redux/features/store.ts
+++ src/redux/features/store.ts
@@ -25,3 +25,6 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+
+export const persistor = persistStore(store)
