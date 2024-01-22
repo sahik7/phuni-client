fb199ab925826c668acdc7a0c4e653e10e022501
sahik7 <sahikahsan7@gmail.com>
2024-01-22 11:58:18 +0600

    persistConfig

--- src/redux/features/store.ts
+++ src/redux/features/store.ts
@@ -2,6 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
 import { baseApi } from '../api/baseApi'
 import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+const persistConfig = {
+    key="auth",
+    storage
+}
+
 
 export const store = configureStore({
     reducer: {
