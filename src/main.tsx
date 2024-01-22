2bf57a3d702ee36a6446acf5b7073b84bc988442
sahik7 <sahikahsan7@gmail.com>
2024-01-22 12:12:12 +0600

    persistor

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import { RouterProvider } from "react-router-dom"
 import router from './routes/routes.tsx'
 import { Provider } from 'react-redux'
-import { store } from './redux/features/store.ts'
+import { persistor, store } from './redux/features/store.ts'
 import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
