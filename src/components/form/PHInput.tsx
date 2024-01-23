68995affce45478d6577aadf06ba647ded831b67
sahik7 <sahikahsan7@gmail.com>
2024-01-23 14:30:29 +0600

    label added

--- src/components/form/PHInput.tsx
+++ src/components/form/PHInput.tsx
@@ -1,10 +1,11 @@
 import { useFormContext } from "react-hook-form";
 
-const PHInput = ({ type, name }) => {
+const PHInput = ({ type, name, label }) => {
 
     const { register } = useFormContext()
     return (
         <div>
+            {label ? label : null}
             <input type={type} id={name} {...register(name)} />
         </div>
     );
